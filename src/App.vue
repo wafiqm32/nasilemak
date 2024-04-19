80225ec30764458a1f52b00de1a3e5b6f4101710
Wafiq <22ftt1516@student.pb.edu.bn>
2024-04-19 21:57:23 +0800

    added fix typo

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
 
     
     </div>
-     <p class="text-red-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl ">HELLO SIR BAHIT MY NAME IS WAFIQ MY STUDENT IS 22FTT1516. I AM 19 YEARS OLD AND I CAN CODE</p>
-     <p class="text-yellow-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl">PLEASE EAT YOUR PILLS AND DOWNLOAD PILLEASE</p>
+  <p class="text-red-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl border border-black bg-white p-2">HELLO SIR BAHIT MY NAME IS WAFIQ MY STUDENT ID IS 22FTT1516. I AM 19 YEARS OLD AND I CAN CODE</p>
+<p class="text-yellow-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl border border-black bg-white p-2">PLEASE EAT YOUR PILLS AND DOWNLOAD PILLEASE</p>
      <button>I am a button, i dont do anything</button>
-       <img src="./assets/pilleaten.png" alt="Vue logo" class="w-40 md:w-auto rounded-xl" />     <img src="./assets/pilleaten.png" alt="Vue logo" class="w-full md:w-20 rounded-xl" />
+     <img src="./assets/pilleaten.png" alt="Vue logo" class="w-20 md:w-20 rounded-xl" />
   </template>
 
 <style scoped>
