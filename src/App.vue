60ba8584b4549ac7b228082786b8e30e83b46572
Wafiq <22ftt1516@student.pb.edu.bn>
2024-04-19 21:51:40 +0800

    added dist 6.0

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
      <p class="text-red-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl ">HELLO SIR BAHIT MY NAME IS WAFIQ MY STUDENT IS 22FTT1516. I AM 19 YEARS OLD AND I CAN CODE</p>
      <p class="text-yellow-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl">PLEASE EAT YOUR PILLS AND DOWNLOAD PILLEASE</p>
      <button>I am a button, i dont do anything</button>
-       <img src="./assets/pilleaten.png" alt="Vue logo" class="w-full md:w-auto rounded-xl" />     <img src="./assets/pilleaten.png" alt="Vue logo" class="w-full md:w-auto rounded-xl" />
+       <img src="./assets/pilleaten.png" alt="Vue logo" class="w-40 md:w-auto rounded-xl" />     <img src="./assets/pilleaten.png" alt="Vue logo" class="w-full md:w-20 rounded-xl" />
   </template>
 
 <style scoped>
