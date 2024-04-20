7d8ef39499b09ad4ad91a634f910e7bdb870cbde
Wafiq <22ftt1516@student.pb.edu.bn>
2024-04-20 10:09:32 +0800

    demo pratice

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     </div>
   <p class="text-red-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl border border-black bg-white p-2">HELLO SIR BAHIT MY NAME IS WAFIQ MY STUDENT ID IS: 22FTT1516. I AM 19 YEARS OLD AND I CAN CODE</p>
 <p class="text-yellow-700 text-xs sm:text-sm md:text-lg lg:text-xl xl:text-2xl border border-black bg-white p-2">PLEASE EAT YOUR PILLS AND DOWNLOAD PILLEASE</p>
-     <button>I am a button, i dont do anything</button>
+     <button>I am a button, i dont do anything, Hello World</button>
      <img src="./assets/pilleaten.png" alt="Vue logo" class="w-20 md:w-20 rounded-xl" />
   </template>
 
